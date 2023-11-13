ea886a382aed03e381a2914228d8a8d2cf8666e7
Rushil54 <127128916+Rushil54@users.noreply.github.com>
2023-11-13 12:33:50 +0530

    The Backend and Ejs files of home

--- app.js
+++ app.js
@@ -1,300 +1,96 @@
 const express = require("express");
 const app = express();
-const { pool } = require("./dbConfig");
-const bcrypt = require("bcrypt");
+const {pool}= require("./dbConfig");
 const session = require("express-session");
 const flash = require("express-flash");
-const passport = require("passport");
-const passportLocal = require("passport-local");
-const morgan = require('morgan');
-
-
-
-const initializePassport = require("./passportConfig");
-initializePassport(passport);
-
+const bodyParser=require("body-parser");
+const morgan = require("morgan");
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(morgan("dev"));
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
-app.use(session({
-    secret: 'secret for now',
-    resave: false,
-    saveUninitialized: false,
-})
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
+let items = [];
 
 
-app.use(flash());
-app.use(express.static("public"))
-
-
-
-app.get('/', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs');
-});
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login');
-});
-app.get('/signup', checkAuthenticated, (req, res) => {
-    res.render('login.ejs');
-});
-
-// passport.authenticate('local', { failureRedirect: '/login' })
-app.get('/secrets', checkAuthenticated, (req, res) => {
-    // if (isAuthenticated) {
-
-    //     res.render('secrets');
-    // }
-    res.render('secrets');
-});
-
-app.get('/logout', checkAuthenticated, (req, res) => {
-    req.logOut((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success_msg", "you have successfully loggedout");
-        res.render('login');
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM expensesheet WHERE user_id=$1 ORDER BY expense_id DESC ;",[1]);
+    items = result.rows;
+console.log(result);
+    res.render("file.ejs", {
+      listTitle: "Today",
+      listItems: items,
     });
-});
-
-app.get('/friends', checkAuthenticated, async (req, res) => {
-    const friendsData = await pool.query(`SELECT *FROM friends WHERE user_id = $1`, [req.user.user_id]);
-    console.log(friendsData.rows)
-
-    console.log(friendsData.rows[0].friends_array);
-    if (notFoundUser) {
-        res.render('friends', { friends: friendsData.rows[0].friends_array, errmsg: "user not found" });
-
-    }
-    res.render('friends', { friends: friendsData.rows[0].friends_array });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
-
-app.get('/delete/:friend', checkAuthenticated, async (req, res) => {
-    const deletingFriend = req.params.friend;
-    const result = await (pool.query(`SELECT friends_array FROM friends where user_id = $1;`, [req.user.user_id]));
-    const Bfriends_array = result.rows[0].friends_array;
-
-
-    let Afriends_array = [];
-    function removeItem(arr, value) {
-        var i = 0;
-        const l = arr.length;
-        while (i < l) {
-            if (arr[i] === value) {
-                arr.splice(i, 1);
-                break;
-            } else {
-                ++i;
-            }
-        }
-        return arr;
-    }
-
-    Afriends_array = removeItem(Bfriends_array, deletingFriend);
-
-
-    await pool.query(`UPDATE friends SET friends_array =$1 WHERE user_id = $2;`, [Afriends_array, req.user.user_id]);
-
-    res.redirect('/friends');
+app.get('/delete/:id',async(req,res)=>{
+const deletingId = req.params.id;
+try{
+  pool.query(`DELETE FROM expensesheet WHERE expense_id = $1;`,[parseInt(deletingId)]);
+}catch(err){
+console.log(err);  
+}
 });
 
+app.get('/edit/:id',async(req,res)=>{
+  const updatingingId = req.params.id;
+  try{
+    pool.query(`UPDATE expensesheet SET amount = $1 category = $2 note = $3 WHERE expense_id = $2;`,[req.body.amount,req.body.category,req.body.note,parseInt(updatingingId)]);
+  }catch(err){
+  console.log(err);  
+  }
+  });
 
-app.get('/groups', checkAuthenticated, (req, res) => {
-    res.render("groupshome");
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  // const amount=parseInt;
+  // items.push({title: item});
+  try {
+    pool.query("INSERT INTO expensesheet (amount,category,user_id,note) VALUES ($1,$2,$3,$4);", [req.body.newAmount,req.body.newCategory,1,req.body.newNote]);
+     res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get('/creategroup', checkAuthenticated, async (req, res) => {
-    function generateRandomNumber() {
-        var minm = 1000000;
-        var maxm = 9999990;
-        return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
-    }
+// app.post("/edit", async (req, res) => {
+//   // const item = req.body.updatedItemTitle;
+//   // const id = req.body.updatedItemId;
 
-    let output = generateRandomNumber();
+//   try {
+//     await pool.query("UPDATE expensesheet SET amount = ($1) WHERE expense_id = $2", [req.body.amount,20]);
+//     res.redirect("/");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-    let result = await pool.query(`SELECT EXISTS (SELECT 1 FROM group_details WHERE group_code = $1);`, [output]);
+// app.delete("/delete", async (req, res) => {
+//   const id = req.params.deleteItemId;
+//   // try {
+//   //   await pool.query("DELETE FROM expensesheet WHERE expense_id = $1", [3,1]);
+//   //   res.redirect("/");
+//   // } catch (err) {
+//   //   console.log(err);
+//   // }
 
-
-
-    while (result.rows[0].exists) {
-        output = generateRandomNumber();
-        result = await pool.query(`SELECT EXISTS (SELECT 1 FROM group_details WHERE group_code = $1);`, [output]);
-    }
-
-    res.render("creategroup", { groupcode: output })
-});
-
-
-
-
-
-
-
-
-app.post('/signup', async (req, res) => {
-    let { name, email, password, password2 } = req.body;
-
-    // console.log({name,email,password,password2});
-    let errors = [];
-
-    if (!name || !email || !password || !password2) {
-        errors.push({ message: "please enter all fields" });
-    }
-    if (password.length < 6) {
-        errors.push({ message: "password should be atleast 6 characters" });
-    }
-    if (password !== password2) {
-        errors.push({ message: "passwords donot match" });
-
-    }
-
-    await pool.query(
-        `SELECT * FROM users
-        WHERE name = $1`, [name], (err, results) => {
-        if (err) {
-            throw err;
-        }
-        if (results.rows.length > 0) {
-            errors.push({ message: "Username is already being used" });
-        }
-    }
-    );
-    if (errors.length > 0) {
-        // console.log('reached here');
-        res.render('login.ejs', { errors })
-    } else {
-        //form validation has passed
-        let hashedPassword = await bcrypt.hash(password, 10);
-        // console.log(hashedPassword);
-
-
-        pool.query(
-            `SELECT * FROM users
-                WHERE email = $1`, [email], (err, results) => {
-            if (err) {
-                throw err;
-            }
-            console.log(results.rows)
-            if (results.rows.length > 0) {
-                errors.push({ message: "email already registered" });
-                res.render('login', { errors });
-            } else {
-                let newUserId;
-                pool.query(
-                    `INSERT INTO users (name, email, password)
-                            VALUES ($1, $2, $3)
-                            RETURNING user_id, password`, [name, email, hashedPassword], (err, results) => {
-                    if (err) {
-                        throw err;
-                    }
-                    console.log("im here");
-                    newUserId = results.rows[0].user_id;
-                    console.log(results.rows[0]);
-                    console.log(results.rows[0].user_id);
-                    console.log(newUserId);
-                    req.flash("success_msg", "you have successfully registered, Please Login");
-                    pool.query(`INSERT INTO friends (user_id)
-                                VALUES ($1)`, [BigInt(newUserId)]);
-                    res.redirect('/')
-                }
-                )
-
-
-            }
-        }
-        )
-    }
-
-    // console.log(errors);
-
-
-});
-
-
-app.post('/login', passport.authenticate('local', {
-    successRedirect: "/secrets",
-    failureRedirect: "/login",
-    failureFlash: true
-}));
-
-let notFoundUser = false;
-app.post('/add-friend', checkAuthenticated, async (req, res) => {
-    const user = req.user;
-    console.log(user);
-    let friendname;
-    const friendExists = await pool.query(`SELECT EXISTS (SELECT 1 FROM users WHERE name = $1);`, [req.body.intofriends]);
-    console.log(friendExists);
-    if (!friendExists.rows[0].exists) {
-        notFoundUser = true;
-        res.render('errorpg', { errmsg: "no user identified of incorrect username", link: "/friends" });
-        return;
-    } else {
-
-        friendname = req.body.intofriends;
-        console.log(friendname);
-        pool.query(` UPDATE friends SET friends_array = friends_array ||$1 WHERE user_id=$2
-                    
-                    RETURNING friends_array;`, [[friendname], parseInt(user.user_id)], (err, results) => {
-            if (err) {
-                throw err;
-            }
-            console.log(results);
-            res.render("friends", { friends: results.rows[0].friends_array });
-
-        });
-    }
-});
-
-
-
-
-
-app.post('/create/:code', (req, res) => {
-
-    if (req.body.groupcode !== req.params.code) {
-        res.render("creategroup", { errmsg: "entered group code not matched", groupcode: req.params.code });
-    } else {
-        pool.query(`INSERT INTO group_details (group_name,host_id,host_name,group_code) VALUES ($1,$2,$3,$4);`, [req.body.groupname, BigInt(req.user.user_id), req.user.name, req.params.code]);
-        res.redirect('/groups');
-    }
-});
-
-
-
-
-app.post('/joingroup',checkAuthenticated,async(req,res)=>{
-
-    
-});
-
-
-
-
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }    
-    res.redirect('/login');
-}    
-function checkNotAuthenticated(req, res, next) {
-    if (!(req.isAuthenticated())) {
-        return next();
-    }    
-    res.redirect('/home');
-}    
-
+//   await pool.query("DELETE FROM expensesheet WHERE expense_id = $1", [id])
+//     .then(result =>{
+//       res.redirect("/");
+//         })
+//     .catch(err =>{
+//       console.log(err);
+//     });
+// });
 
 app.listen(port, () => {
-    console.log(`sever running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
---
+++ queries.sql
@@ -0,0 +1,53 @@
+CREATE TABLE users(
+	user_id BIGSERIAL,
+	name TEXT NOT NULL UNIQUE,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	google_id TEXT,
+	PRIMARY KEY(user_id)
+);
+
+
+CREATE TABLE expensesheet(
+	expenseId BIGSERIAL,
+	user_id BIGINT NOT NULL,
+	amount MONEY,
+	category TEXT NOT NULL,
+	note TEXT,
+	timeStamp TIMESTAMP(0) DEFAULT current_timestamp,
+	PRIMARY KEY(expenseId),
+	FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE friends(
+relation_id BIGSERIAL PRIMARY KEY,
+	user_id BIGINT REFERENCES users(user_id),
+	friends_array TEXT[]
+	
+);
+
+CREATE TABLE groupdetails(
+groupId BIGSERIAL PRIMARY KEY,
+	group_name text not null,
+	host_id bigint references users(user_id),
+	host_name text references users(name),
+	members text[] 
+	
+);
+
+CREATE TABLE group_expensesheet(
+	id BIGSERIAL PRIMARY KEY,
+	group_id bigint references groupdetails(groupid),
+	amount money,
+	payor_id bigint references users(user_id),
+	payor text references users(name),
+	debtors text[],
+	note text,
+	timeStamp TIMESTAMP(0) DEFAULT current_timestamp
+	
+);
+
+INSERT INTO users (username,email,password,google_id)
+VALUES ('Rush', 'lol@mail.com','pass','lol@gmail.com'),('Rama','haha@mail.com','word','haha@gmail.com');
+
+
