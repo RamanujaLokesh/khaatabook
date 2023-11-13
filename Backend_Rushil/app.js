ef52466fe7330277b0f6ea031cd94bcd05a28cd7
Rushil54 <rushil2k4@gmail.com>
2023-11-13 12:29:43 +0530

    the backend and ejs files of homepage

---
+++ Backend_Rushil/app.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const {pool}= require("./dbConfig");
+const session = require("express-session");
+const flash = require("express-flash");
+const bodyParser=require("body-parser");
+const morgan = require("morgan");
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+let items = [];
+
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM expensesheet WHERE user_id=$1 ORDER BY expense_id DESC ;",[1]);
+    items = result.rows;
+console.log(result);
+    res.render("file.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+app.get('/delete/:id',async(req,res)=>{
+const deletingId = req.params.id;
+try{
+  pool.query(`DELETE FROM expensesheet WHERE expense_id = $1;`,[parseInt(deletingId)]);
+}catch(err){
+console.log(err);  
+}
+});
+
+app.get('/edit/:id',async(req,res)=>{
+  const updatingingId = req.params.id;
+  try{
+    pool.query(`UPDATE expensesheet SET amount = $1 category = $2 note = $3 WHERE expense_id = $2;`,[req.body.amount,req.body.category,req.body.note,parseInt(updatingingId)]);
+  }catch(err){
+  console.log(err);  
+  }
+  });
+
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  // const amount=parseInt;
+  // items.push({title: item});
+  try {
+    pool.query("INSERT INTO expensesheet (amount,category,user_id,note) VALUES ($1,$2,$3,$4);", [req.body.newAmount,req.body.newCategory,1,req.body.newNote]);
+     res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// app.post("/edit", async (req, res) => {
+//   // const item = req.body.updatedItemTitle;
+//   // const id = req.body.updatedItemId;
+
+//   try {
+//     await pool.query("UPDATE expensesheet SET amount = ($1) WHERE expense_id = $2", [req.body.amount,20]);
+//     res.redirect("/");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// app.delete("/delete", async (req, res) => {
+//   const id = req.params.deleteItemId;
+//   // try {
+//   //   await pool.query("DELETE FROM expensesheet WHERE expense_id = $1", [3,1]);
+//   //   res.redirect("/");
+//   // } catch (err) {
+//   //   console.log(err);
+//   // }
+
+//   await pool.query("DELETE FROM expensesheet WHERE expense_id = $1", [id])
+//     .then(result =>{
+//       res.redirect("/");
+//         })
+//     .catch(err =>{
+//       console.log(err);
+//     });
+// });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
