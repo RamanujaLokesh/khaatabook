1dc58c62f0a9357fa120c7ec8bcc857afe1d84cd
Harishnandre <127136728+Harishnandre@users.noreply.github.com>
2023-11-14 12:02:14 +0530

    Front end of Home page

---
+++ Home Page/home.js
@@ -0,0 +1,57 @@
+let lightModeEl = document.getElementById("lightMode")
+let darkModeEl = document.getElementById("darkMode")
+let contrastModeEl = document.getElementById("contrastMode")
+lightModeEl.onclick = function() {
+    lightModeEl.classList.add("d-none")
+    darkModeEl.classList.remove("d-none")
+}
+darkModeEl.onclick = function() {
+    darkModeEl.classList.add("d-none")
+    contrastModeEl.classList.remove("d-none")
+}
+contrastModeEl.onclick = function() {
+    contrastModeEl.classList.add("d-none")
+    lightModeEl.classList.remove("d-none")
+}
+
+
+let homeSectionNoteCardEl = document.getElementById("homeSectionNoteCard")
+let homeSectionNoteCardNoteEl = document.getElementById("homeSectionNoteCardNote")
+let homeSectionNoteCardNoteInputEl = document.getElementById("homeSectionNoteCardNoteInput")
+let homeSectionNoteCardIconEl = document.getElementById("homeSectionNoteCardIcon")
+let homeSectionNoteCardPenEl = document.getElementById("homeSectionNoteCardPen")
+let homeSectionNoteCardDeleteEl = document.getElementById("homeSectionNoteCardDelete")
+homeSectionNoteCardIconEl.onclick = function() {
+    if (homeSectionNoteCardIconEl.classList.contains("fa-chevron-down")) {
+        homeSectionNoteCardIconEl.classList.remove("fa-chevron-down")
+        homeSectionNoteCardIconEl.classList.add("fa-angle-up")
+        homeSectionNoteCardNoteEl.classList.remove("d-none")
+        homeSectionNoteCardPenEl.classList.remove("d-none")
+        homeSectionNoteCardDeleteEl.classList.remove("d-none")
+        homeSectionNoteCardPenEl.onclick = function() {
+            if (homeSectionNoteCardPenEl.classList.contains("fa-pen")) {
+                homeSectionNoteCardPenEl.classList.remove("fa-pen")
+                homeSectionNoteCardPenEl.classList.add("fa-check")
+                homeSectionNoteCardNoteInputEl.classList.remove("d-none")
+                homeSectionNoteCardNoteEl.classList.add("d-none")
+                homeSectionNoteCardNoteInputEl.value = homeSectionNoteCardNoteEl.textContent
+            } else {
+                homeSectionNoteCardPenEl.classList.add("fa-pen")
+                homeSectionNoteCardPenEl.classList.remove("fa-check")
+                homeSectionNoteCardNoteInputEl.classList.add("d-none")
+                homeSectionNoteCardNoteEl.classList.remove("d-none")
+                homeSectionNoteCardNoteEl.innerHTML = homeSectionNoteCardNoteInputEl.value
+            }
+
+        }
+    } else {
+        homeSectionNoteCardIconEl.classList.add("fa-chevron-down")
+        homeSectionNoteCardIconEl.classList.remove("fa-angle-up")
+        homeSectionNoteCardNoteEl.classList.add("d-none")
+        homeSectionNoteCardPenEl.classList.add("d-none")
+        homeSectionNoteCardDeleteEl.classList.add("d-none")
+        homeSectionNoteCardNoteInputEl.classList.add("d-none")
+    }
+
+
+}
